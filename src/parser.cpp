a79127ed09e6fe47ae58d296a56eea8502229764
mnoj <jon85ma@gmail.com>
2026-03-12 00:30:23 +0700

    improve parser + tests

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,28 +1,23 @@
 #include "parser.h"
 
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>
-
-#include <iostream>
-#include <cassert>
-#include <stdexcept>
 #include <string>
 #include <string_view>
 
 
 
-void HttpReq::Parser(std::string_view message) {
+void HttpRes::Parser(std::string_view message) {
   // 1. Get Status-Line
-
-  auto get_line = [&message]() -> std::string_view {
+  auto get_line = [&message, this]() -> std::string_view {
       auto line_end = message.find("\r\n");
       if (line_end == std::string_view::npos) {
           // handles nc that only sends a \n
           line_end = message.find('\n');
           if (line_end == std::string_view::npos) {
-              std::cerr << "Incomplete line" << std::endl;
-              return "";
+              res_message.append(message.substr(0, line_end));
+              if (parsing_state != ParseState::kBody) {
+                // no proper handling if start-line or header-line is cut in the middle
+                throw std::runtime_error("Invalid line");
+              }
           }
           auto line = message.substr(0, line_end);
           message.remove_prefix(line_end + 1);
@@ -41,55 +36,65 @@ void HttpReq::Parser(std::string_view message) {
     status_code = status_line.substr(first_space + 1, second_space - first_space - 1);
     status_message = status_line.substr(second_space + 1);
     parsing_state = ParseState::kHeaders;
+
+    res_message.append("HTTP/1.1 200 Ok\r\n");
   }
 
   // 2. Get message-headers
   if (parsing_state == ParseState::kHeaders) {
     std::string_view header;
-    std::string key;
     while(!(header = get_line()).empty()) {
         auto separator = header.find(": ");
+        std::string key;
         std::string value;
         if (separator == std::string_view::npos) {
             if (header[0] != ' ') {
               throw std::runtime_error("Wrong header format for multiline headers");
             }
-            auto prev_value = headers.at(key);
-            value = prev_value.append(header);
-  
+            res_message.append(header);
         } else {
             key = header.substr(0, separator);
             value = header.substr(separator + 2);
+            res_message.append(key)
+                       .append(": ")
+                       .append(value)
+                       .append("\r\n");
+            if (key == "Content-Length") {
+              content_length = std::stoi(value);
+            }
         }
-        headers.insert({key, value});
     }
-    auto content_length = headers.find("Content-Length");
-    if (content_length != headers.end()) {
-      if (std::stoi(content_length->second) > 0) {
-        parsing_state = ParseState::kBody;
-      } else {
-        parsing_state = ParseState::kEnd;
-      }
+    res_message.append("\r\n");
+    if (content_length > 0) {
+      parsing_state = ParseState::kBody;
+    } else {
+      parsing_state = ParseState::kEnd;
     }
   }
 
   // 3. Get message-body
   if (parsing_state == ParseState::kBody) {
-    body = message;
-
-    parsing_state = ParseState::kEnd;
+    res_message.append(message);
+    content_length -= message.size();
+    if (content_length <= 0) {
+      parsing_state = ParseState::kEnd;
+    }
   }
 }
 
-const std::string& HttpReq::ContentLength() const {
-  return headers.at("Content-Length");
-}
-const std::string& HttpReq::StatusCode() const noexcept {
+const std::string& HttpRes::StatusCode() const noexcept {
   return status_code;
 }
-const std::string& HttpReq::StatusMessage() const noexcept {
+
+const std::string& HttpRes::StatusMessage() const noexcept {
   return status_message;
 }
-const std::string& HttpReq::Body() const noexcept {
-  return body;
+
+const std::string& HttpRes::Response() const noexcept {
+  return res_message;
 }
+
+void HttpRes::Clear() noexcept {
+  res_message.clear();
+}
+
--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -1,11 +1,14 @@
-#include <gtest/gtest.h>
-#include <string>
 #include "parser.h"
 
+#include <string>
 
-TEST(Parser, ReqWithoutBody)
+#include <gtest/gtest.h>
+#include <string_view>
+
+
+TEST(Parser, ExtractStatusLine)
 { 
-  HttpReq test;
+  HttpRes test;
   std::string http =
       "HTTP/1.0 200 OK\r\nServer: SimpleHTTP/0.6 Python/3.12.1\r\nDate: Mon,\r\n 02 Mar 2026 11:08:55 GMT\r\nContent-type: text/html\r\n\n";
   test.Parser(http);
@@ -13,9 +16,10 @@ TEST(Parser, ReqWithoutBody)
   EXPECT_EQ(test.StatusMessage(), "OK");
 }
 
+
 TEST(Parser, ReqWithBody) {
-  HttpReq test2;
-  std::string http2 = 
+  HttpRes test;
+  std::string http = 
     "HTTP/1.0 200 OK\n"
     "Server: SimpleHTTP/0.6 Python/3.12.1\n"
     "Date: Mon, 02 Mar 2026 11:08:55 GMT\n"
@@ -33,7 +37,14 @@ TEST(Parser, ReqWithBody) {
     "<marquee>Welcome to my website</marquee>"
     "</body>"
     "</html>";
-  std::string body =
+  std::string expected = 
+    "HTTP/1.1 200 Ok\r\n"
+    "Server: SimpleHTTP/0.6 Python/3.12.1\r\n"
+    "Date: Mon, 02 Mar 2026 11:08:55 GMT\r\n"
+    "Content-type: text/html\r\n"
+    "Content-Length: 145\r\n"
+    "Last-Modified: Tue, 21 Feb 2023 02:05:00 GMT\r\n"
+    "\r\n"
     "<html>"
     "<head>"
     "<link rel=\"stylesheet\" href=\"styles.css\">"
@@ -44,9 +55,56 @@ TEST(Parser, ReqWithBody) {
     "<marquee>Welcome to my website</marquee>"
     "</body>"
     "</html>";
-  test2.Parser(http2);
-  int content_length = stoi(test2.ContentLength());
-  EXPECT_EQ(content_length, body.length());
-  EXPECT_EQ(content_length, test2.Body().size());
-  EXPECT_EQ(test2.Body(), body);
+  test.Parser(http);
+  EXPECT_EQ(test.Response(), expected);
+}
+
+
+TEST(Parser, BrokenReqWithBody) {
+  HttpRes test;
+  std::string http_part_1 = 
+    "HTTP/1.0 200 OK\n"
+    "Server: SimpleHTTP/0.6 Python/3.12.1\n"
+    "Date: Mon, 02 Mar 2026 11:08:55 GMT\n"
+    "Content-type: text/html\n"
+    "Content-Length: 145\n";
+  std::string http_part_2 =
+    "Last-Modified: Tue, 21 Feb 2023 02:05:00 GMT\n"
+    "\n"
+    "<html>"
+    "<head>"
+    "<link rel=\"stylesheet\" href=\"styles.css\">"
+    "</head>"
+    "<body>"
+    "<img src=\"marc.jpeg\">"
+    "<br>"
+    "<marquee>Welcome to my website</marquee>"
+    "</body>"
+    "</html>";
+  std::string expected = 
+    "HTTP/1.1 200 Ok\r\n"
+    "Server: SimpleHTTP/0.6 Python/3.12.1\r\n"
+    "Date: Mon, 02 Mar 2026 11:08:55 GMT\r\n"
+    "Content-type: text/html\r\n"
+    "Content-Length: 145\r\n"
+    "Last-Modified: Tue, 21 Feb 2023 02:05:00 GMT\r\n"
+    "\r\n"
+    "<html>"
+    "<head>"
+    "<link rel=\"stylesheet\" href=\"styles.css\">"
+    "</head>"
+    "<body>"
+    "<img src=\"marc.jpeg\">"
+    "<br>"
+    "<marquee>Welcome to my website</marquee>"
+    "</body>"
+    "</html>";
+  EXPECT_ANY_THROW(test.Parser(std::string_view(http_part_1.data(), http_part_1.size())));
+  std::string full_res;
+  full_res.append(test.Response());
+  EXPECT_EQ(test.StatusCode(), "200");
+  test.Clear();
+  test.Parser(std::string_view(http_part_2.data(), http_part_2.size()));
+  full_res.append(test.Response());
+  EXPECT_EQ(full_res, expected);
 }
